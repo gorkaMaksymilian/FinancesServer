7fff68815c2974007ee473933ba426ea90ecb339
GorkaMaksymilian <gorkamaks@gmail.com>
2022-07-31 17:15:33 +0200

    Expense repository created

---
+++ Data/ExpenseRepo.cs
@@ -0,0 +1,46 @@
+using FinancesServer.Models.Expenses;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinancesServer.Data
+{
+    public class ExpenseRepo : IExpenseRepo
+    {
+        private readonly FinancesDbContext _context;
+
+        public ExpenseRepo(FinancesDbContext context)
+        {
+            _context = context;   
+        }
+
+        public async Task CreateExpense(Expense exp)
+        {
+            if (exp is null)
+                throw new ArgumentNullException(nameof(exp));
+
+            await  _context.AddAsync(exp);
+        }
+
+        public void DeleteExpense(Expense exp)
+        {
+            if (exp is null)
+                throw new ArgumentNullException(nameof(exp));
+            
+            _context.Expenses.Remove(exp);
+        }
+
+        public async Task<IEnumerable<Expense>> GetAllExpenses()
+        {
+            return await _context.Expenses.ToListAsync();
+        }
+
+        public async Task<Expense?> GetExpenseById(int id)
+        {
+            return await _context.Expenses.FirstOrDefaultAsync(exp => exp.Id == id);
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
