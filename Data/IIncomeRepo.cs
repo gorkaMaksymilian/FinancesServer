651b53c44638b5f9e3baf4b62b16be4c8b7b700a
GorkaMaksymilian <gorkamaks@gmail.com>
2022-07-31 17:10:42 +0200

    Income repository created

--- Data/IIncomeRepo.cs
+++ Data/IIncomeRepo.cs
@@ -5,7 +5,7 @@ namespace FinancesServer.Data
     public interface IIncomeRepo
     {
         Task SaveChanges();
-        Task<Income> GetIncomeById(int id);
+        Task<Income?> GetIncomeById(int id);
         Task<IEnumerable<Income>> GetAllEarnings();
         Task CreateIncome(Income inc);
 
---
+++ Data/IncomeRepo.cs
@@ -0,0 +1,46 @@
+using FinancesServer.Models.Earnings;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinancesServer.Data
+{
+    public class IncomeRepo : IIncomeRepo
+    {
+        private readonly FinancesDbContext _context;
+
+        public IncomeRepo(FinancesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateIncome(Income inc)
+        {
+            if (inc is null)
+                throw new ArgumentNullException(nameof(inc));
+
+            await _context.AddAsync(inc);
+        }
+
+        public void DeleteIncome(Income inc)
+        {
+            if (inc is null)
+                throw new ArgumentNullException(nameof(inc));
+
+            _context.Earnings.Remove(inc);
+        }
+
+        public async Task<IEnumerable<Income>> GetAllEarnings()
+        {
+            return await _context.Earnings.ToListAsync();
+        }
+
+        public async Task<Income?> GetIncomeById(int id)
+        {
+            return await _context.Earnings.FirstOrDefaultAsync(inc => inc.Id == id);
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
