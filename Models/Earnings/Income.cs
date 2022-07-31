3dd64fcf2666940a6a632157c3713fbcd66f74b1
GorkaMaksymilian <gorkamaks@gmail.com>
2022-07-31 15:48:20 +0200

    Model of Income object created

---
+++ Models/Earnings/Income.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FinancesServer.Models.Earnings
+{
+    public class Income
+    {
+        [Key]
+        public int Id {get;set;}
+
+        [Required]
+        public DateTime? Date {get;set;}
+
+        [Required]
+        [StringLength(50)]
+        public string? Description {get;set;}
+
+        [Required]
+        public decimal Amount {get;set;}
+
+        [Required]
+        public IncomeCategoryEnum Category {get;set;}
+    }
+}
---
+++ Models/Earnings/IncomeCategoryEnum.cs
@@ -0,0 +1,12 @@
+namespace FinancesServer.Models.Earnings
+{
+    public enum IncomeCategoryEnum
+    {
+        Salary,
+        Overtime,
+        Intrest,
+        Gifts,
+        Capital,
+        Dividend
+    }
+}
