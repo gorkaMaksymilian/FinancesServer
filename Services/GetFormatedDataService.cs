d0f9cbc24384ee672eace2d8db53854e773d7102
GorkaMaksymilian <gorkamaks@gmail.com>
2022-08-03 14:53:35 +0200

    Code to get monthly expenses implemented

--- Services/GetFormatedDataService.cs
+++ Services/GetFormatedDataService.cs
@@ -1,6 +1,6 @@
 using FinancesServer.Models.Dashboard;
 using FinancesServer.Models.Earnings;
-
+using FinancesServer.Models.Expenses;
 
 namespace FinancesServer.Services
 {
@@ -27,6 +27,20 @@ namespace FinancesServer.Services
                    {
                         Amount = inc.Sum(item => item.Amount),
                         Category = inc.Key.ToString()
+                   });
+        }
+
+        public async Task<IEnumerable<MonthlyItem>> GetMonthlyExpenses(int month, int year)
+        {
+            var data = await _http.GetFromJsonAsync<List<Expense>>($"{BaseUri}api/expense");
+
+            return data!.Where(exp => exp.Date >= new DateTime(year, month, 1) &&
+                   exp.Date <= new DateTime(year, month, DateTime.DaysInMonth(year, month)))
+                   .GroupBy(exp => exp.Category)
+                   .Select(exp => new MonthlyItem
+                   {
+                        Amount = exp.Sum(item => item.Amount),
+                        Category = exp.Key.ToString()
                    });
         }
     }
--- Services/IGetFormatedDataService.cs
+++ Services/IGetFormatedDataService.cs
@@ -5,5 +5,6 @@ namespace FinancesServer.Services
     public interface IGetFormatedDataService
     {
         Task<IEnumerable<MonthlyItem>> GetMonthlyEarnings(int month, int year);
+        Task<IEnumerable<MonthlyItem>> GetMonthlyExpenses(int month, int year);
     }
 }
