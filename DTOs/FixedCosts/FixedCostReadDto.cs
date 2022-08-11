d85d1693b2751e735c292502021c481d1b570a7a
GorkaMaksymilian <gorkamaks@gmail.com>
2022-08-11 18:16:19 +0200

    Dto properties created

--- DTOs/FixedCosts/FixedCostReadDto.cs
+++ DTOs/FixedCosts/FixedCostReadDto.cs
@@ -1,4 +1,14 @@
+using FinancesServer.Models.FixedCosts;
+using FinancesServer.Models.Shared;
+
 namespace FinancesServer.DTOs.FixedCosts
 {
-    
+    public class FixedCostReadDto 
+    {
+        public int Id {get;set;}
+        public string? Description {get;set;}
+        public decimal Amount {get;set;} 
+        public FixedCostCategoryEnum Category { get; set; }
+        public MonthEnum MonthOfFirstPayment {get;set;} 
+    }
 }
--- DTOs/FixedIncome/FixedIncomeReadDto.cs
+++ DTOs/FixedIncome/FixedIncomeReadDto.cs
@@ -1,4 +1,14 @@
+using FinancesServer.Models.FixedIncome;
+using FinancesServer.Models.Shared;
+
 namespace FinancesServer.DTOs.FixedIncome
 {
-    
+    public class FixedIncomeReadDto
+    {
+        public int Id {get;set;}
+        public string? Description {get;set;}
+        public decimal Amount {get;set;} 
+        public FixedIncomeCategoryEnum Category { get; set; }
+        public MonthEnum MonthOfFirstPayment {get;set;} 
+    }
 }
