8e024cb7180da88ebdd644c2a3578a0cdeb3a18b
GorkaMaksymilian <gorkamaks@gmail.com>
2022-08-25 14:15:46 +0200

    Data annotations added

--- Models/Shared/User.cs
+++ Models/Shared/User.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FinancesServer.Models.Shared
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string? Username { get; set; }
+        [Required]
         public string? Email { get; set; }
+        [Required]
         public string? Password {get; set;}
+        [Required]        
         public AuthLevelEnum AuthLevel { get; set; }
     }
 }
